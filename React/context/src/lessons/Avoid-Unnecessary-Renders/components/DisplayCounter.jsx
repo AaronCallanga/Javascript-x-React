8fa047c7429785147c63680c73c488371d8268ab
AaronCallanga <aaroncallanga01@gmail.com>
2025-11-24 17:55:12 +0800

    DisplayCounter

---
+++ React/context/src/lessons/Avoid-Unnecessary-Renders/components/DisplayCounter.jsx
@@ -0,0 +1,19 @@
+// src/DisplayCounter.js
+import React from 'react';
+import { useCounterValue } from '../CounterContext';
+
+function DisplayCounter() {
+  // This component consumes the VALUE.
+  // It WILL re-render every time the 'count' changes.
+  const count = useCounterValue();
+
+  console.log('Rendering DisplayCounter'); // Check your console!
+
+  return (
+    <div style={{ padding: '10px', border: '1px solid blue' }}>
+      Current Count: <strong>{count}</strong>
+    </div>
+  );
+}
+
+export default DisplayCounter;
